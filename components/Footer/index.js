3ee3cf5c8637746223b783f0d9d3d37b5f593583
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-01-20 17:00:24 -0500

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,7 +8,7 @@ const Footer = props => {
         <S.Container>
             <S.WidthLimit>
                 <span>
-                    © {year} - <a href="https://wweb.dev?utm_source=convert2svg" target="_blank" rel="noopener">wweb.dev</a>
+                    © {year} - <a href="https://abclinks.vercel.app" target="_blank" rel="noopener">uvacoder</a>
                 </span>
                 <a href="/privacy">
                     Privacy Policy
