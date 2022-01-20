9a98ffabbfdb72ec0680812c1dee2536dcfc2ca9
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-01-20 17:12:13 -0500

    Update styled.js

--- components/Header/styled.js
+++ components/Header/styled.js
@@ -4,6 +4,7 @@ import { gradient, maxWidth } from '../../styles/constants'
 export const Container = styled.header`
     background: ${gradient};
     padding: 20px 0;
+    font-size: 22px;
     margin-bottom: 30px;
 `
 export const WidthLimit = styled.div`
